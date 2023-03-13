24dafa96dbef445664b2a36e61577d1c6cbf6d4d
242556 Wojciech Tomicki <242556@edu.p.lodz.pl>
2023-03-13 14:14:40 +0100

    Initial commit with base Hello World aplication and another file with test case

---
+++ Project/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    class MainClass {         
+        static void Main(string[] args)
+        {
+            MainClass cla = new MainClass();
+            Console.WriteLine(cla.HelloWorld());
+        }
+
+        public string HelloWorld()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ Project/Test.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld;
+using NUnit.Framework;
+
+public class Test
+{
+    [Test]
+    public void testHelloWorld()
+    {
+        MainClass TestClass = new MainClass();
+        Assert.True(TestClass.HelloWorld()=="Hello World!");
+    }
+} 
