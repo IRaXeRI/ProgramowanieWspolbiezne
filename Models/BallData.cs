9896607b897ba78fa7b77270f2fdac2461a1501a
IRaXeRI <wojciech.tomicki52@gmail.com>
2023-04-16 12:18:44 +0200

    Adding Data Layer and Logic Layer // BallData and BoardLogic are abstract classes used to store requierd data // BallLogic and BoardLogic are classe that inherits from data layer and provides business logic for application

---
+++ Models/BallData.cs
@@ -0,0 +1,48 @@
+﻿namespace TPW_E1.Models;
+
+public abstract class BallData
+{
+    private int coordX;
+    private int coordY;
+    private int velocityX;
+    private int velocityY;
+
+    public int CoordX {
+        get { return coordX; }
+        set {
+            if (coordX == value)
+                return;
+            coordX = value;
+        }
+    }
+    
+    public int CoordY {
+        get { return coordY; }
+        set {
+            if (coordY == value)
+                return;
+            coordY = value;
+        }
+    }
+    
+    public int VelX {
+        get { return velocityX; }
+        set {
+            if (velocityX == value)
+                return;
+            velocityX = value;
+        }
+    }
+    
+    public int VelY {
+        get { return velocityY; }
+        set {
+            if (velocityY == value)
+                return;
+            velocityY = value;
+        }
+    }
+
+    public abstract void update(int boardLength, int boardWidth);
+
+}
---
+++ Models/BoardLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Project;
+
+namespace TPW_E1.Models;
+
+public class BoardLogic : BoardData
+{
+    public BoardLogic(int initLenght, int initWidth) : base(initLenght, initWidth)
+    {
+    }
+    
+    public override void addBall() {
+        Random rand = new Random();
+        int cX = rand.Next(0, Length);
+        int cY = rand.Next(0, Width);
+        int vX = rand.Next(1, 10);
+        int vY = rand.Next(1, 10);
+        BallData newBall = new BallLogic(cX, cY, vX, vY);
+        points.Add(newBall);
+    }
+    
+    public override void addBall(BallData ball) {
+        points.Add(ball);
+    }
+    
+    public override bool[,] update() {
+        bool[,] answer = new bool[Length + 1,Width + 1];
+        for (int i = 0; i < points.Count; i++) {
+            points.ElementAt(i).update(Length, Width);
+            int x = points.ElementAt(i).CoordX;
+            int y = points.ElementAt(i).CoordY;
+            answer[x, y] = true;
+        }
+        return answer;
+    }
+}
